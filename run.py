f1a82995b1e42f78703477afbf30f77d46c37d7f
wanwu <2350686113@qq.com>
2020-01-02 12:32:46 +0800

    添加下载单曲方法

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ class Musics(object):
 
     _server = 'http://127.0.0.1:3000'
     _filepath = os.path.dirname(__file__)
+
     @property
     def login(self):
         """
@@ -44,11 +45,10 @@ class Musics(object):
         """
         获取歌单id
         :param uid: 用户 ID
-        :return: 歌单列表 [{'id': 974291055, 'name': 'wanwu_z喜欢的音乐'}, {'id': 3109278173, 'name': '每日歌曲推荐(2019.12.08)'}]
+        :return: 歌单列表
         """
-        self.uid = uid
         m_data = []
-        url = self._server + '/user/playlist?uid={}'.format(self.uid)
+        url = self._server + '/user/playlist?uid={}'.format(uid)
         response = self.login.get(url)
         if response.status_code == 200:
             data = json.loads(response.text)
@@ -61,47 +61,26 @@ class Musics(object):
                 m_data.append(record)
             return m_data
 
-    def get_sid(self, mid):
+    def get_song_info(self, mid):
         """
         获取歌单中歌曲ID
         :param mid: 歌单ID
-        :return: 歌曲ID列表 # [{'id': 176430, 'name': '爱上你是一个错', 'br': 320000}, {'id': 1295824261, 'name': '又何苦', 'br': 320000}]
+        :return: 歌曲信息
         """
-        self.mid = mid
         song_list = []
-        url = self._server + '/playlist/detail?id={}'.format(self.mid)
+        url = self._server + '/playlist/detail?id={}'.format(mid)
         response = self.login.get(url)
         if response.status_code == 200:
             data = json.loads(response.text)
-            mlist = data['playlist']['tracks']
-            for i in mlist:
-                if i['h']:
-                    record = {
-                        'id': i['id'],
-                        'name': i['name'],
-                        'singer': i['ar'][0]['name'],
-                        'br': i['h']['br'],
-                        'url': ''
-                    }
-                    song_list.append(record)
-                elif i['m']:
-                    record = {
-                        'id': i['id'],
-                        'name': i['name'],
-                        'singer': i['ar'][0]['name'],
-                        'br': i['m']['br'],
-                        'url': ''
-                    }
-                    song_list.append(record)
-                else:
-                    record = {
-                        'id': i['id'],
-                        'name': i['name'],
-                        'singer': i['ar'][0]['name'],
-                        'br': i['l']['br'],
-                        'url': ''
-                    }
-                    song_list.append(record)
+            playlist = data['playlist']['tracks']
+            for i in playlist:
+                record = {
+                    'id': i['id'],
+                    'name': i['name'],
+                    'singer': i['ar'][0]['name'],
+                    'url': ''
+                }
+                song_list.append(record)
             return song_list
 
     def vail_song(self, sid):
@@ -110,8 +89,7 @@ class Musics(object):
         :param sid: 歌曲 ID
         :return:
         """
-        self.sid = sid
-        url = self._server + '/check/music?id={}'.format(self.sid)
+        url = self._server + '/check/music?id={}'.format(sid)
         response = self.login.get(url)
         if response.status_code == 200:
             data = json.loads(response.text)
@@ -123,45 +101,59 @@ class Musics(object):
         :param sid: 歌曲ID
         :return:
         """
-        self.sid = sid
-        url = self._server + '/song/url?id={}'.format(self.sid)
+        url = self._server + '/song/url?id={}'.format(sid)
         response = self.login.get(url)
         if response.status_code == 200:
-            data = json.loads(response.text)
-            return data['data']
+            data = json.loads(response.text)['data'][0]
+            url = data['url']
+            br = data['br']
+            return url, br
 
     def make_new_song_list(self, mid):
         """
         提取有效的歌曲，组合成有版权的歌单
         :return:
         """
-        self.mid = mid
         new_song_list = []
         print('正在提取有效歌曲...')
-        song_mapping = self.get_sid(self.mid)
+        song_mapping = self.get_song_info(mid)
         for song in song_mapping:
             status = self.vail_song(song['id'])
-            if status == True:
-                url = self.get_song_url(song['id'])
+            if status:
+                url, br = self.get_song_url(song['id'])
                 record = {
                     'id': song['id'],
                     'name': song['name'],
                     'singer': song['singer'],
-                    'br': song['br'],
-                    'url': url[0]['url']
+                    'br': br,
+                    'url': url,
                 }
                 print('添加歌曲 "{}" '.format(song['name']))
                 new_song_list.append(record)
         print('所有有效歌曲添加完成...')
         return new_song_list
 
-    def downloads(self, mid):
+    def download(self, sid):
         """
-        下载音乐
+        下载单首歌曲
         :return:
         """
-        self.mid = mid
-        songs = self.make_new_song_list(self.mid)
+        url, br = self.get_song_url(sid)
+        name = input('请输入歌名：')
+        singer = input("请输入歌手：")
+        filename = name + '-' + singer + '.mp3'
+        response = requests.get(url)
+        if response.status_code == 200:
+            print('正在下载歌曲 {} ...'.format(filename))
+            with open(os.path.join(self._filepath, 'musics', filename), 'wb') as f:
+                f.write(response.content)
+
+    def downloads(self, mid):
+        """
+        下载歌单所有音乐
+        :return:
+        """
+        songs = self.make_new_song_list(mid)
         print('开始下载歌曲...')
         for song in songs:
             name = song['name'] + '-' + song['singer'] + '.mp3'
@@ -179,7 +171,8 @@ if __name__ == '__main__':
     ret = m.status
     uid = ret['profile']['userId']  # 获取用户 ID
     music_mapping = m.get_mid(uid)  # 获取歌单列表
-    m.downloads(974291055)
+    m.download(1413087236)
+    # m.downloads(3109278173)
     # song_mapping = m.make_new_song_list(974291055)     # 获取某歌单中的歌曲列表
     # for i in song_mapping:
     #     print(i)
